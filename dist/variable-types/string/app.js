e5446b6f26a1498f2d92d8f122d098fac012ea85
Gean634N <gean.a.l.c@gmail.com>
2022-01-15 15:13:32 -0400

    string type

---
+++ dist/variable-types/string/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let firstName = 'Gean';
+console.log('meu primeiro nome é ', firstName);
+let lastName = "Gonçalves";
+console.log('meu ultimo nome é ', lastName);
+let fullName = `${firstName} ${lastName}`;
+console.log(`
+  Meu nome completo é ${firstName} ${lastName}
+`);
---
+++ variable-types/string/app.ts
@@ -0,0 +1,13 @@
+//Ex01 single quotes
+let firstName: string = 'Gean'
+console.log('meu primeiro nome é ',firstName);
+
+//Ex02 double quoutes
+let lastName: string = "Gonçalves"
+console.log('meu ultimo nome é ',lastName);
+
+//Ex03 back ticks
+let fullName: string = `${firstName} ${lastName}`
+console.log(`
+  Meu nome completo é ${firstName} ${lastName}
+`);
