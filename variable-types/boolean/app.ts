35e0c49726b78cf853a3afec29bf4b11de9bd686
Gean634N <gean.a.l.c@gmail.com>
2022-01-15 13:13:26 -0400

    boolean type

---
+++ variable-types/boolean/app.ts
@@ -0,0 +1,15 @@
+// Ex01
+let completedTask: boolean = true;
+let pendingTask: boolean = false;
+
+console.log(completedTask);
+console.log(pendingTask);
+
+// Ex02
+let completed: boolean = false;
+
+if (completed) {
+  console.log('A tarefa foi concluida');  
+} else {
+  console.log('Tarefa pendente');
+}
