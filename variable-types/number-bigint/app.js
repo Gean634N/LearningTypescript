65805636933e01b1bfd1c6c9eae5f9abc39f9dd4
Gean634N <gean.a.l.c@gmail.com>
2022-01-15 13:30:58 -0400

    number type

---
+++ variable-types/number-bigint/app.js
@@ -0,0 +1,9 @@
+// Ex01 - number
+var num0 = 23.0;
+var num1 = 0x78CF;
+var num2 = 383;
+var num3 = 49;
+console.log("Number - Ponto Flutuante...: ".concat(num0, " tipo ").concat(typeof (num0)));
+console.log("Number - Hexadecimal...: ".concat(num1, " tipo ").concat(typeof (num1)));
+console.log("Number - Octal...: ".concat(num2, " tipo ").concat(typeof (num2)));
+console.log("Number - Binario...: ".concat(num3, " tipo ").concat(typeof (num3)));
---
+++ variable-types/number-bigint/app.ts
@@ -0,0 +1,11 @@
+// Ex01 - number
+
+let num0: number = 23.0;
+let num1: number = 0x78CF;
+let num2: number = 0o577;
+let num3: number = 0b110001;
+
+console.log(`Number - Ponto Flutuante...: ${num0} tipo ${typeof(num0)}`);
+console.log(`Number - Hexadecimal...: ${num1} tipo ${typeof(num1)}`);
+console.log(`Number - Octal...: ${num2} tipo ${typeof(num2)}`);
+console.log(`Number - Binario...: ${num3} tipo ${typeof(num3)}`);
